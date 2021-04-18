8accb335322ec7c400d0518b6f276a88f3a94163
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-04-18 13:59:12 -0500

    order routes started restructuring orders

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -13,7 +13,24 @@ module.exports = () => {
   orderRoutes.get('/:alias', async (req, res, next) => {
     logger.log('silly', 'getting orders by alias');
     const orders = await orderService.getCompletedOrdersByAlias(req.params.alias);
-    console.log(orders);
+
+    // console.log(
+    //   orders.map((order) => ({
+    //     _id: order._id,
+    //     gifts: Object.values(order.cart.items),
+    //     alias: order.toJSON().cart.alias,
+    //     tender: {
+    //       amount: order.toJSON().cart.totalPrice,
+    //       currency: order.cart.alias.currency,
+    //       afterConversion: order.toJSON().convertedCart
+    //         ? order.toJSON().cashFlow.toConnect.amount
+    //         : null,
+    //       noteToWisher: order.noteToWisher,
+    //       fromLine: orders[0].toJSON().buyerInfo.fromLine,
+    //       paidOn: order.paidOn,
+    //     },
+    //   }))[0]
+    // );
     res.send(orders);
   });
   orderRoutes.post(
