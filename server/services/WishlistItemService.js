ed44a37ccf7568d7c56c6f8b38471445caed3e9b
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-05-27 11:53:53 -0500

    delete doesn't delete image if item was purchased

--- server/services/WishlistItemService.js
+++ server/services/WishlistItemService.js
@@ -204,11 +204,11 @@ class WishlistItemService {
   }
 
   /**
-   * deletes a wishlist item
+   * deletes a wishlist item entirely
    *
    *@param {string} id the wishlist item id
    *
-   * @returns {object} deleted wishlist
+   * @returns {object} deleted wishlist item from the
    */
   async deleteWishlistItem(id) {
     let item;
