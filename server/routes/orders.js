ac8517c9da0d91d77664ecf43fc7be123e1d83d3
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-06-21 20:55:54 -0500

    order routes secure

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -3,13 +3,39 @@ const logger = require('../lib/logger');
 const { ApplicationError } = require('../lib/Error');
 const OrderModel = require('../models/Order.Model');
 const OrderService = require('../services/OrderService');
+const { authLoggedIn } = require('./middlewares');
 
 const orderService = new OrderService(OrderModel);
 const orderRoutes = express.Router();
 const ThankYouEmail = require('../lib/email/ThankYouEmail');
 
+async function authUserOwnsAliasInParam(req, res, next) {
+  logger.log('silly', `authorizing user owns resource...`);
+
+  // should authorize that user of alias is req.user
+  if (!req.user.aliases.includes(req.params.alias)) {
+    return res.status(403).send({
+      message: `User doesn't own alias.`,
+    });
+  }
+  return next();
+}
+const authUserOwnsOrder = (errorMessage) => async (req, res, next) => {
+  try {
+    req.order = await orderService.getOrder({ _id: req.params.id });
+    if (req.user.aliases[0].toString() !== req.order.alias.toString())
+      return res.status(403).send("User doesn't have permission.");
+  } catch (err) {
+    return next(new ApplicationError({ err }, `errorMessage`));
+  }
+  return next();
+};
+const throwIfAlreadyMarkedAsSeen = async (req, res, next) => {
+  if (req.order.seen) return res.status(409).send({ message: 'Wish already marked as seen.' });
+  return next();
+};
 module.exports = () => {
-  orderRoutes.get('/:alias', async (req, res, next) => {
+  orderRoutes.get('/:alias', authLoggedIn, authUserOwnsAliasInParam, async (req, res, next) => {
     logger.log('silly', 'getting orders by alias');
     const orders = await orderService.getCompletedOrdersByAlias(req.params.alias);
 
@@ -32,81 +58,57 @@ module.exports = () => {
     }));
     res.send(restructuredOrders);
   });
-  orderRoutes.get(
-    '/new/:alias',
-    async (req, res, next) => {
-      if (!req.user) return res.status(401).send('No user logged in.');
-      return next();
-    },
-    async (req, res, next) => {
-      logger.log('silly', 'getting new orders by alias');
-      const newOrders = await orderService.getNewOrdersByAlias({ _id: req.params.alias });
-      res.status(200).send(newOrders);
-    }
-  );
+  orderRoutes.get('/new/:alias', authLoggedIn, authUserOwnsAliasInParam, async (req, res, next) => {
+    logger.log('silly', 'getting new orders by alias');
+    const newOrders = await orderService.getNewOrdersByAlias({ _id: req.params.alias });
+    res.status(200).send(newOrders);
+  });
   orderRoutes.patch(
     '/seen/:id',
+    authLoggedIn,
+    authUserOwnsOrder("Couldn't mark as seen."),
+    throwIfAlreadyMarkedAsSeen,
     async (req, res, next) => {
-      if (!req.user) return res.status(401).send('No user logged in.');
-      try {
-        req.order = await orderService.getOrder({ _id: req.params.id });
-        if (req.user.aliases[0].toString() !== req.order.alias.toString())
-          return res.status(403).send("User doesn't have permission.");
-      } catch (err) {
-        return next(new ApplicationError({}, `Couldn't mark as read: ${err}`));
-      }
-      return next();
-    },
-    async (req, res, next) => {
-      if (req.order.seen) return res.status(409).send({ message: 'Wish already marked as seen.' });
-      return next();
-    },
-    async (req, res, next) => {
-      logger.log('silly', 'marking wish as seen');
+      logger.log('silly', 'marking wish as seen and note to wisher as read');
       req.order.seen = true;
+      req.order.noteToWisher.read = true;
       await req.order.save();
-      res.status(201).send();
+      res.status(200).send();
     }
   );
 
-  orderRoutes.patch(
-    '/read/:id',
-    async (req, res, next) => {
-      if (!req.user) return res.status(401).send('No user logged in.');
-      try {
-        req.order = await orderService.getOrder({ _id: req.params.id });
-        if (req.user.aliases[0].toString() !== req.order.alias.toString())
-          return res.status(403).send("User doesn't have permission.");
-      } catch (err) {
-        return next(new ApplicationError({}, `Couldn't mark as read: ${err}`));
-      }
-      return next();
-    },
-    async (req, res, next) => {
-      if (req.order.noteToWisher && req.order.noteToWisher.read)
-        return res.status(409).send({ message: 'Note to wisher already read.' });
-      return next();
-    },
-    async (req, res, next) => {
-      logger.log('silly', 'marking note to wisher as read');
-      req.order.noteToWisher.read = true;
-      await req.order.save();
-      res.status(201);
-    }
-  );
+  // read is not used in the front end, seen is used and we mark as read then
+  // orderRoutes.patch(
+  //   '/read/:id',
+  //   authLoggedIn,
+  //   authUserOwnsOrder,
+  //   async (req, res, next) => {
+  //     if (!req.user) return res.status(401).send('No user logged in.');
+  //     try {
+  //       req.order = await orderService.getOrder({ _id: req.params.id });
+  //       if (req.user.aliases[0].toString() !== req.order.alias.toString())
+  //         return res.status(403).send("User doesn't have permission.");
+  //     } catch (err) {
+  //       return next(new ApplicationError({}, `Couldn't mark as read: ${err}`));
+  //     }
+  //     return next();
+  //   },
+  //   async (req, res, next) => {
+  //     if (req.order.noteToWisher && req.order.noteToWisher.read)
+  //       return res.status(409).send({ message: 'Note to wisher already read.' });
+  //     return next();
+  //   },
+  //   async (req, res, next) => {
+  //     logger.log('silly', 'marking note to wisher as read');
+  //     req.order.noteToWisher.read = true;
+  //     await req.order.save();
+  //     res.status(201);
+  //   }
+  // );
   orderRoutes.patch(
     '/reply/:id',
-    async (req, res, next) => {
-      if (!req.user) return res.status(401).send('No user logged in.');
-      try {
-        req.order = await orderService.getOrder({ _id: req.params.id });
-        if (req.user.aliases[0].toString() !== req.order.alias.toString())
-          return res.status(403).send("User doesn't have permission.");
-      } catch (err) {
-        return next(new ApplicationError({}, `Couldn't reply to tender ${err}`));
-      }
-      return next();
-    },
+    authLoggedIn,
+    authUserOwnsOrder("Couldn't reply."),
     async (req, res, next) => {
       if (req.order.noteToTender && req.order.noteToTender.message)
         return res.status(409).send({ message: 'Note to tender already sent.' });
@@ -131,7 +133,7 @@ module.exports = () => {
           await req.order.save();
         }
       } catch (err) {
-        return next(new ApplicationError({}, `Couldn't reply to tender ${err}`));
+        return next(new ApplicationError({ err }, `Couldn't reply to tender`));
       }
       return res.status(200).send({ messageSent: message });
     }
