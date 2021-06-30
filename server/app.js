d6f29376e6bf9f1ea933b11fafb319a90c6f9233
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-06-30 14:06:08 -0500

    baseurl update

--- server/app.js
+++ server/app.js
@@ -23,12 +23,12 @@ const logger = require('./lib/logger');
 const routes = require('./routes');
 
 if (process.env.NODE_ENV === 'production') {
-  process.env.FRONT_BASEURL = 'https://www.wishtender.com/';
-  process.env.API_BASEURL = 'https://api.wishtender.com/';
+  process.env.FRONT_BASEURL = 'https://www.wishtender.com';
+  process.env.API_BASEURL = 'https://api.wishtender.com';
 } else if (process.env.NODE_ENV === 'development' && process.env.REMOTE === 'true') {
   // staging environment is set up this way ^
-  process.env.FRONT_BASEURL = 'https://staging.wishtender.com/';
-  process.env.API_BASEURL = 'https://api-staging.wishtender.com/';
+  process.env.FRONT_BASEURL = 'https://staging.wishtender.com';
+  process.env.API_BASEURL = 'https://api-staging.wishtender.com';
 } else {
   process.env.FRONT_BASEURL = 'http://localhost:3000';
   process.env.API_BASEURL = 'http://localhost:4000';
