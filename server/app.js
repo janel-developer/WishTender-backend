24c2ba82ee084682d9b7cae99e850c86e588c3c7
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-08-19 14:00:05 -0500

    country spelling

--- server/app.js
+++ server/app.js
@@ -161,13 +161,14 @@ module.exports = (config) => {
   // --------------------------------------
 
   // app.use((req, res, next) => {
-  //   req.session.p = 1;
-  //   console.log('req.headers: ', req.headers);
-  //   console.log('req.body: ', req.body);
-  //   console.log('req.cookies: ', req.cookies);
-  //   console.log('req.user: ', req.user);
+  //   //   req.session.p = 1;
+  //   //   console.log('req.headers: ', req.headers);
+  //   //   console.log('req.body: ', req.body);
+  //   //   console.log('req.cookies: ', req.cookies);
+  //   //   console.log('req.user: ', req.user);
 
   //   res.on('close', async () => {
+  //     console.log('req', req);
   //     console.log('res.statusCode', res.statusCode);
   //     console.log('res.statusMessage', res.statusMessage);
   //     console.log('res.headers', res._headers);
