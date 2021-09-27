7b61f3d945e6e5f3ea1026a8aeab7bd869e4a900
DashBarkHuss <dashiellbarkhuss@gmail.com>
2021-09-27 17:29:34 -0500

    random

--- server/lib/email/ConfirmationEmail.js
+++ server/lib/email/ConfirmationEmail.js
@@ -11,7 +11,7 @@ class ConfirmationEmail extends Email {
     const subject = `Please confirm your email`;
     const html = `Please confirm your email: <a href = "${
       process.env.API_BASEURL + emailActivationUrl
-    }">Confirm</a>`;
+    }">Confirm</a> <p>If you get an error 'Cannot read property '0' of null', try the link on desktop Chrome. This error has been reported on Android when using mobile Chrome.</p>`;
     const email = process.env.CONFIRM_EMAIL;
     const from = `WishTender Wishlist <${email}>`;
     const pass = process.env.CONFIRM_PASSWORD;
